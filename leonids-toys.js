8adddad7fa1984a14e1c5595c5062910fbf70066
Duley Williams <duleygrafix@gmail.com>
2021-11-06 12:17:25 -0500

    second

--- leonids-toys.js
+++ leonids-toys.js
@@ -38,7 +38,27 @@ const retroToys = [
         faction: "Strawberry Land",
         releaseYear: 1982,
         originalPrice: 7.99
-    }
+    },
+    {
+        id: 5,
+        toyLine:"G.I. Joe: A Real American Hero",
+        name: "Duke",
+        manufacturer: "Hasbro",
+        generation: "G1",
+        faction: "G.I. Joe",
+        releaseYear: 1982,
+        originalPrice: 3.99
+    },
+    {
+        id: 6,
+        toyLine:"Rainbow Brite",
+        name: "Rainbow Brite",
+        manufacturer: "Mattel",
+        generation: "G1",
+        faction: "Rainbow Land",
+        releaseYear: 1982,
+        originalPrice: 10.99
+    },
 ]
 
 // console.log(retroToys)
@@ -56,8 +76,8 @@ const retroToys = [
 // }
 
 
-const careBear = {
-        id: 5,
+    const careBears = {   
+        id: 7,
         toyLine:"Care Bears",
         name: "Tenderheart",
         manufacturer: "Kenner",
@@ -65,14 +85,54 @@ const careBear = {
         faction: "Care-a-Lot",
         releaseYear: 1982,
         originalPrice: 12.99
-}
+    }
 
-retroToys.push(careBear)
+    retroToys.push(careBears)
+
+    const liteBrite = {
+        id: 8,
+        toyLine:"Lite Brite",
+        name: "Lite Brite",
+        manufacturer: "Kenner",
+        generation: "NA",
+        faction: "Light It Up",
+        releaseYear: 1982,
+        originalPrice: 15.99
+    }
+    retroToys.push(liteBrite)
+
+// console.log(retroToys)
+
+
+
+
+
+
+
+// for (const retroToy of retroToys) {
+//     retroToy.originalPrice = retroToy.originalPrice * (1 + .05)
+//     console.log(`The ${retroToy.originalPrice}`)
+// }
+
+
+
+// for (const retroToy of retroToys) {
+//     retroToys.originalPrice = retroToys.originalPrice * (1 + .05)
+//     console.log(`${retroToy.toyLine}, ${retroToy.name} was released in ${retroToy.releaseYear} which cost $${retroToy.originalPrice}.`)
+// }
+
+const retroToyToFind = 1
+
+// const retroToyToFind = retroToys[1].id
+// console.log(retroToyToFind)
 
 for (const retroToy of retroToys) {
-    retroToy.originalPrice = retroToy.originalPrice * (1 + .05);
-}
 
-for (const retroToy of retroToys) {
-    console.log(`${retroToy.toyLine}, ${retroToy.name} was released in ${retroToy.releaseYear} which cost $${retroToy.originalPrice}.`)
+    if (retroToy.id === retroToyToFind) {
+        retroToy.originalPrice = retroToy.originalPrice * (1 + .05)
+        console.log(`The ${retroToy.manufacturer} ${retroToy.name} cost ${retroToy.originalPrice} dollars. It was released in ${retroToy.releaseYear}`)
+    }
 }
+// return retroToyToFind
+
+
